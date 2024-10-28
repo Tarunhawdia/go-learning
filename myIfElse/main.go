9e472331f27c5875f3092e5d4f156916dca271c1
Tarun Hawdia <tarunhawdiya112@gmail.com>
2024-10-28 07:22:36 +0530

    learned ifelse

---
+++ myIfElse/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+
+	loginCount := 10
+	var result string
+
+	if loginCount < 10 {
+		result = "Regular User"
+	} else if loginCount > 10 {
+		result = "Watch out"
+	} else {
+		result = "Exactly 10 login count"
+	}
+	fmt.Println(result)
+
+	if num := 3; num < 10 {
+		fmt.Println("num is less than 10")
+	} else {
+		fmt.Println("num is not less than 10")
+	}
+
+}
